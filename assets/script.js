f15f214bae6bc384247e4d6e0680f8d011ad5ffc
Oliver Saxon <olisaxon@Olivers-Mac-mini.local>
2021-03-09 12:55:57 +0000

    LAYOUT: Updated some HTML elements and layout.

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,8 @@ var wrong = document.getElementById("wrong");
 var endMsg = document.querySelector(".end-message");
 var question = document.querySelector(".question");
 var optionA = document.getElementById("a");
+var optionB = document.getElementById("b");
+var optionC = document.getElementById("c");
 var questionNumber = 0;
 
 var questions = [
@@ -12,6 +14,14 @@ var questions = [
         options: ["Is it this...","This...","Or this..."],
         answer: "a",
     },
+    {
+        question: "question 2",
+        options: ["This", "that", "or this"],
+        answer: "b",
+    },
+    {
+        question: "question 3"
+    }
 ];
 
 var score = 0;
@@ -34,6 +44,7 @@ function setTime() {
 
   function wrongAnswer() {
       timeLeft = timeLeft - 10;
+      // mainLoop
   }
 
 function endQuiz() {
@@ -44,17 +55,27 @@ function endQuiz() {
     }
 }
 
-function startQuiz(){
+function mainLoop(){
     console.log("started")
     setTime()
     var currentQuestion = questions[questionNumber];
     var currentOptionA = currentQuestion.options[0];
+    var currentOptionB = currentQuestion.options[1];
+    var currentOptionC = currentQuestion.options[2];
     question.textContent = currentQuestion.question;
     optionA.textContent = currentOptionA;
+    optionB.textContent = currentOptionB;
+    optionC.textContent = currentOptionC;
 
+}
+
+function correctAnswer() {
+    console.log("correct");
+    // Q number + 1
+    // mainLoop
 }
 
 
 // event listeners
-startBtn.addEventListener("click", startQuiz)
+startBtn.addEventListener("click", mainLoop)
 wrong.addEventListener("click", wrongAnswer)
