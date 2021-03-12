f8874fe95bce05f81d1f98792175fc351f1e6481
Oliver Saxon <olisaxon@Olivers-Mac-mini.local>
2021-03-12 12:49:04 +0000

    UPDATE: New layout design using  Bootstrap. Implemented jQuery in script.

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,12 @@
 // DOM elements
-let timer = document.querySelector(".time")
-let titleEl = document.querySelector("body > div > header > h1");
-let startBtn = document.getElementById("start");
-let questionEl = document.querySelector(".question");
-let questionBoxEl = document.querySelector(".question-box");
-let optionA = document.getElementById("a");
-let optionB = document.getElementById("b");
-let optionC = document.getElementById("c");
-let options = document.querySelector(".options")
-let endGameMsgEl = document.querySelector(".end-game");
+let timer = $(".time");
+let titleEl = $(".title")
+let startBtn = $(".start-btn");
+let questionEl = $(".question");
+let questionListEl = $(".question-list");
+let optionA = $("a");
+let optionB = $("b");
+let optionC = $("c");
 // end of DOM elements
 
 
@@ -36,6 +34,8 @@ let questions = [
 ];
 
 
+
+let gameWon = false;
 let questionNumber = 0;
 let currentQuestion = questions[questionNumber];
 let currentOptionA = currentQuestion.options[0];
@@ -46,54 +46,39 @@ let currentOptionC = currentQuestion.options[2];
 let score = 0;
 let timeLeft = 100;
 
-function setTime() {
-    console.log("Stage: setTime")
-    timer.textContent = timeLeft;
-    let timerInterval = setInterval(function() {
-      timeLeft--;
-      timer.textContent = timeLeft;
-  
-      if(timeLeft === 0 || questionNumber > questions.length) {
-        // Stops execution of action at set interval
-        clearInterval(timerInterval);
-      }
-  
-    }, 1000);
-  }
-
-  function wrongAnswer() {
-    
-  }
 
 function endQuiz() {
     console.log("Stage: endQuiz")
-    options.style.display = 'none';
-    questionBoxEl.style.display = 'none';
+    questionListEl.hide();
     if(timeLeft > 0){
         score = timeLeft * 10;
-        endGameMsgEl.textContent = "Congratualtions on finishing the quiz! Your score is " + timeLeft;
     } else {
-        endMsg.textContent = "Time's up you lose!!!"
+        return;
     }
 }
 
 function setUp(){
     console.log("Stage: setUp")
-    startBtn.style.display = 'none';
-    titleEl.style.display = 'none';
-    options.style.display = 'flex';
-    setTime();
+    startBtn.addClass("disabled");
+    questionListEl.show();
+    timer.text("s remaining");
+    let timerInterval = setInterval(function() {
+      timeLeft--;
+      timer.text(timeLeft + "s remaining")
+  
+      if(timeLeft === 0 || questionNumber == questions.length) {
+        // Stops execution of action at set interval
+        clearInterval(timerInterval);
+        timer.hide();
+      }
+  
+    }, 1000);
     renderQuestion();
 }
 
 
-function correctAnswer() {
-    
-    
-}
 
 function renderQuestion(){
-    console.log("Stage: renderQuestion")
     if(questionNumber === questions.length){
         endQuiz();
     } else {
@@ -102,34 +87,23 @@ function renderQuestion(){
         optionA.textContent = currentQuestion.options[0];
         optionB.textContent = currentQuestion.options[1];
         optionC.textContent = currentQuestion.options[2];
-    }
-
-}
-
-function mainLoop(){
-    console.log("Stage: mainLoop")
-    if(timeLeft == 0 || questionNumber == questions.length){
-        endQuiz();
-    } else {
-        renderQuestion();
-    }
-}
+    };
+};
 
 
 
 // event listeners
-startBtn.addEventListener("click", setUp)
 
-options.addEventListener("click", function(event){
+startBtn.on("click", setUp)
+// startBtn.addEventListener("click", setUp)
+
+questionListEl.on("click", function(event){
     event.preventDefault()
     plAns = event.target.id;
-    console.log(plAns);
     if(plAns === currentQuestion.answer){
-        console.log("Stage: correctAnswer")
         questionNumber += 1;
         renderQuestion();
     } else {
-        console.log("Stage: wrongAns")
         timeLeft = timeLeft - 10;
         renderQuestion();
     }
